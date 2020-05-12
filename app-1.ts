e26254748d34c4575ad7446e0ba297acafbced72
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-11 22:22:41 -0500

    Actiones y reducer básico

--- app-1.ts
+++ app-1.ts
@@ -1,1 +1,20 @@
-console.log('Hola mundo!!!');
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+function reducer(state = 10, action: Action) {
+
+  if (action.type === 'INCREMENTAR') {
+    state += 1;
+  }
+
+  return state;
+
+}
+
+const incrementadorAction: Action = {
+  type: 'INCREMENTAR'
+}
+
+console.log(reducer(10, incrementadorAction));
